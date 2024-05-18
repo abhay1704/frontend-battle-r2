b58bb6bfe883b25cf64c1f933c86f720969db4c6
Abhay Pratap Singh <abhaypratapninth@gmail.com>
2024-05-18 23:49:27 +0530

    Complete Home Page

---
+++ script.js
@@ -0,0 +1,19 @@
+// const homeBkd = document.querySelector("#home-bkd");
+
+// ["load", "resize"].forEach((e) => {
+//   window.addEventListener(e, () => {
+//     const width = homeBkd.width;
+//     const height = homeBkd.height;
+//     const ctx = homeBkd.getContext("2d");
+//     console.log(width, height);
+
+//     ctx.fillStyle = "black";
+//     ctx.beginPath();
+//     ctx.moveTo(0, 0);
+//     ctx.lineTo(0, 0.6 * height);
+//     ctx.arc(0, 1.5, 350, Math.PI / 9, (10 * Math.PI) / 9);
+//     ctx.moveTo(width, 0);
+//     ctx.closePath();
+//     ctx.fill();
+//   });
+// });
